e4026d1bcb06412d8257fdcc7d2ad99741398553
Bryan Romero <109822721+Bryan-Romero@users.noreply.github.com>
2023-01-18 16:40:17 -0700

    Formato fecha y hora corregido

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,21 +1,22 @@
 export const formDateOfEntry = (date: string) => {
   const dateOfEntry = new Date(date);
-  return `${dateOfEntry.getDate()}/${
-    dateOfEntry.getMonth() + 1 < 10
-      ? `0${dateOfEntry.getMonth() + 1}`
-      : dateOfEntry.getMonth() + 1
-  }/${dateOfEntry.getFullYear()} ${dateOfEntry.getHours()}:${
-    dateOfEntry.getMinutes() < 10
-      ? `0${dateOfEntry.getMinutes()}`
-      : dateOfEntry.getMinutes()
+  return `${dateOfEntry.getUTCDate()}/${
+    dateOfEntry.getUTCMonth() + 1 < 10
+      ? `0${dateOfEntry.getUTCMonth() + 1}`
+      : dateOfEntry.getUTCMonth() + 1
+  }/${dateOfEntry.getUTCFullYear()} ${dateOfEntry.getUTCHours()}:${
+    dateOfEntry.getUTCMinutes() < 10
+      ? `0${dateOfEntry.getUTCMinutes()}`
+      : dateOfEntry.getUTCMinutes()
   }`;
 };
 
 export const formExpirationDate = (date: string) => {
   const expirationDate = new Date(date);
-  return `${expirationDate.getDate()}/${
-    expirationDate.getMonth() + 1 < 10
-      ? `0${expirationDate.getMonth() + 1}`
-      : expirationDate.getMonth() + 1
-  }/${expirationDate.getFullYear()}`;
+  console.log(expirationDate.getUTCDate())
+  return `${expirationDate.getUTCDate()}/${
+    expirationDate.getUTCMonth() + 1 < 10
+      ? `0${expirationDate.getUTCMonth() + 1}`
+      : expirationDate.getUTCMonth() + 1
+  }/${expirationDate.getUTCFullYear()}`;
 };
